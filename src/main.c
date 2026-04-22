799f90cff3d1dcb078ff397ee605b98de3cf2d4b
RuabeeTu <46233485+Kyaron233@users.noreply.github.com>
2026-04-22 11:23:27 +0000

    feat:完善加入apikey的功能

--- src/main.c
+++ src/main.c
@@ -14,16 +14,18 @@ static struct option long_options[] = {
     {"output", required_argument, 0, 'O'},
     {"username", required_argument, 0, 'U'},
     {"api", required_argument, 0, 'A'},
+    {"secret", required_argument, 0, 'S'},
     {0, 0, 0, 0}
 };
 // 我把用户名和文件名拿出来了 感觉不能放在栈上
 char username[200]; // 200应该够了吧。。
 char output_filename[200]; // 也是200。注意这个文件名可能是存在的也可能是不存在的
 char api_key[200];
+char secret[200];
 int main(int argc, char *argv[]) {
     int opt;
 
-    while((opt = getopt_long(argc, argv, "hO:U:A:", long_options, NULL)) != -1) {
+    while((opt = getopt_long(argc, argv, "hO:U:A:S:", long_options, NULL)) != -1) {
         switch(opt) {
             case 'h':
                 printhelp();
@@ -40,6 +42,10 @@ int main(int argc, char *argv[]) {
             case 'A':
                 strncpy(api_key, optarg, sizeof(api_key) - 1);
                 api_key[sizeof(api_key) - 1] = '\0';
+                break;
+            case 'S':
+                strncpy(secret, optarg, sizeof(secret) - 1);
+                secret[sizeof(secret) - 1] = '\0';
                 break;
             default:
                 fprintf(stderr, "未知选项：%c\n。使用--help查看帮助信息", opt);
@@ -58,6 +64,20 @@ int main(int argc, char *argv[]) {
 
         if (api_key[0] == '\0') {
             log_message(WARNING, "API密钥为空，部分数据将无法获取");
+        }
+    }
+
+    if (secret[0] == '\0') {
+        printf("请输入您的Secret：（获取方式请参考https://codeforces.com/apiHelp）");
+        fflush(stdout);
+
+        if (fgets(secret, sizeof(secret), stdin) == NULL) {
+            secret[0] = '\0';
+        }
+        secret[strcspn(secret, "\r\n")] = '\0';
+
+        if (secret[0] == '\0') {
+            log_message(WARNING, "Secret为空，部分数据将无法获取");
         }
     }
     // 这里的logger_init主要是能否成功打开日志文件，失败了就只能输出到控制台了
--- src/utils/arg_utils.c
+++ src/utils/arg_utils.c
@@ -10,6 +10,8 @@ int printhelp(){
     printf("  -h, --help            显示帮助信息\n");
     printf("  -O, --output FILE     指定输出文件名\n");
     printf("  -U, --username USER   指定Codeforces用户名\n");
+    printf("  -A, --api KEY         指定API密钥\n");
+    printf("  -S, --secret SECRET   指定API Secret\n");
     printf("  --mutiple-users FILE  从文件中读取多个用户名，要求每行一个用户名\n");
     return 0;
 }
