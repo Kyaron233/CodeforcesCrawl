5c1801f9a6d2fd872853c2f62302cd8933ffec25
RuabeeTu <46233485+Kyaron233@users.noreply.github.com>
2026-04-22 09:50:42 +0000

    feat:(AI)支持输入密钥

--- src/main.c
+++ src/main.c
@@ -13,15 +13,17 @@ static struct option long_options[] = {
     {"help", no_argument, 0, 'h'},
     {"output", required_argument, 0, 'O'},
     {"username", required_argument, 0, 'U'},
+    {"api", required_argument, 0, 'A'},
     {0, 0, 0, 0}
 };
 // 我把用户名和文件名拿出来了 感觉不能放在栈上
 char username[200]; // 200应该够了吧。。
 char output_filename[200]; // 也是200。注意这个文件名可能是存在的也可能是不存在的
+char api_key[200];
 int main(int argc, char *argv[]) {
     int opt;
 
-    while((opt = getopt_long(argc, argv, "hO:U:", long_options, NULL)) != -1) {
+    while((opt = getopt_long(argc, argv, "hO:U:A:", long_options, NULL)) != -1) {
         switch(opt) {
             case 'h':
                 printhelp();
@@ -35,13 +37,31 @@ int main(int argc, char *argv[]) {
                 strncpy(username, optarg, sizeof(username) - 1);
                 username[sizeof(username) - 1] = '\0';
                 break;
+            case 'A':
+                strncpy(api_key, optarg, sizeof(api_key) - 1);
+                api_key[sizeof(api_key) - 1] = '\0';
+                break;
             default:
                 fprintf(stderr, "未知选项：%c\n。使用--help查看帮助信息", opt);
                 return 1;
         }
     }
+
+    if (api_key[0] == '\0') {
+        printf("请输入您的API密钥：（获取方式请参考https://codeforces.com/apiHelp）");
+        fflush(stdout);
+
+        if (fgets(api_key, sizeof(api_key), stdin) == NULL) {
+            api_key[0] = '\0';
+        }
+        api_key[strcspn(api_key, "\r\n")] = '\0';
+
+        if (api_key[0] == '\0') {
+            log_message(WARNING, "API密钥为空，部分数据将无法获取");
+        }
+    }
     // 这里的logger_init主要是能否成功打开日志文件，失败了就只能输出到控制台了
     // 日志放在log/log.txt，按照规范输出日志。使用a模式打开文件，所有的内容都追加到文件末尾
-    
+    return 0;
 
 }
