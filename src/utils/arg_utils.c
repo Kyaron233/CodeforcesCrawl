5d0876a8961b1b2a72af558b7e13d48c62bd981d
RuabeeTu <2407007304@qq.com>
2026-05-19 15:59:21 +0800

    doc:修改help的内容

--- src/utils/arg_utils.c
+++ src/utils/arg_utils.c
@@ -8,10 +8,7 @@ int printhelp(){
     printf("Usage: ./crawler [OPTIONS]\n");
     printf("Options:\n");
     printf("  -h, --help            显示帮助信息\n");
-    printf("  -O, --output FILE     指定输出文件名\n");
     printf("  -U, --username USER   指定Codeforces用户名\n");
-    printf("  -A, --api KEY         指定API密钥\n");
-    printf("  -S, --secret SECRET   指定API Secret\n");
-    printf("  --mutiple-users FILE  从文件中读取多个用户名，要求每行一个用户名\n");
+    printf("  -M                    多用户，提供一个json列表或者每行一个用户名的列表\n");
     return 0;
 }
